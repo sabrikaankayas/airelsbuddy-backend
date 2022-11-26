8deca114cc4a1ad93c027a6c317c50cfc5589c65
Sabri Kaan Kayaş <104498582+sabrikaankayas@users.noreply.github.com>
2022-11-26 05:47:22 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,9 @@ const app = express()
 //Middleware
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
-app.use(cors())
+app.use(cors(
+    {origin: ["http://localhost:3000", "https://airelsbuddy.onrender.com"]
+}))
 app.use("/api/times", timeRoutes)
 app.use("/api/tasks", taskRoutes)
 
